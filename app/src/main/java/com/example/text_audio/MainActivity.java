39ad8e1d524f00a94601f1d466a1a74468c00526
sanonapp <mitelson070@gmail.com>
2023-06-07 13:21:28 -0400

    first commit

---
+++ app/src/main/java/com/example/text_audio/MainActivity.java
@@ -0,0 +1,184 @@
+package com.example.text_audio;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.Resources;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageButton btnSpeak;
+    private TextView tvText;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+    private final int CHECK_CODE = 0x1;
+    private final int SHORT_DURATION = 1000;
+
+    private Button launchPrompt, readText;
+    private MediaPlayer player;
+    private Speaker speaker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        launchPrompt = (Button) findViewById(R.id.launchPrompt);
+        launchPrompt.setOnClickListener(promptListener);
+        btnSpeak = findViewById(R.id.btnSpeak);
+        tvText = findViewById(R.id.tvText);
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"en-us");
+                try {
+                    startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getApplicationContext(), "Désolé, votre appareil ne supporte pas d'entrée vocale...", Toast.LENGTH_SHORT).show();
+                    a.printStackTrace();
+                }
+
+            }
+        });
+
+        readText = (Button) findViewById(R.id.readText);
+        readText.setOnClickListener(readListener);
+
+        checkTTS();
+    }
+
+
+    private void checkTTS(){
+        Intent check = new Intent();
+        check.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(check, CHECK_CODE);
+    }
+
+    View.OnClickListener promptListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            promptSpeechInput();
+        }
+    };
+
+    View.OnClickListener readListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            speakOut();
+        }
+    };
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault().getDisplayLanguage());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Vous pouvez parler ...");
+
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(), "Désolé, votre appareil ne supporte pas d'entrée vocale...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void speakOut() {
+        if(!speaker.isSpeaking()) {
+            speaker.speak(readFile(R.raw.martin_luther_king));
+            speaker.pause(SHORT_DURATION);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    tvText.setText(text.get(0));
+                    String result = text.get(0);
+
+                    Pattern ff = Pattern.compile("Final|fantasy");
+                    if (ff.matcher(result).find()) {
+                        try {
+                            preparePlayer("ffx-victory.mp3");
+                            player.start();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                break;
+            }
+            case CHECK_CODE: {
+                if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                    speaker = new Speaker(this);
+                } else {
+                    Intent install = new Intent();
+                    install.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                    startActivity(install);
+                }
+            }
+            default:
+                break;
+        }
+    }
+
+    public void preparePlayer(String path) throws IOException {
+        AssetFileDescriptor afd = getAssets().openFd(path);
+        player = new MediaPlayer();
+        player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+        player.prepare();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        checkTTS();
+    }
+
+    @Override
+    protected void onStop() {
+        if(player != null) {
+            if(player.isPlaying()) {
+                player.stop();
+            }
+        }
+        speaker.destroy();
+        super.onStop();
+    }
+
+    private String readFile(int rawfile) {
+        String result = new String();
+        try {
+            Resources res = getResources();
+            InputStream input_stream = res.openRawResource(rawfile);
+
+            byte[] b = new byte[input_stream.available()];
+            input_stream.read(b);
+            result = new String(b);
+        } catch (Exception e) {
+            Log.e("readFile", e.getMessage());
+        }
+        return result;
+    }
+
+}
